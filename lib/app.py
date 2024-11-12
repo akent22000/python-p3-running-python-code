4249d4b731ec64fcc6914aa932b4cc1aeecf5c56
amandalocher <akent22000@gmail.com>
2024-11-12 09:38:10 -0500

    completed lab

---
+++ lib/app.py
@@ -0,0 +1,13 @@
+# lib/app.py
+# print("Hello world!")
+# print("Hello sun!")
+# print("Hello sky!")
+
+# print("Hello world!", end=" ")
+# print("Hello sun!", end="!! ")
+# print("Hello sky!", end="!!!\n")
+
+# print("Hello sky!", end="!!!\n")
+
+print("Hello World!", end=" ")
+print("Pass this test, please.", end="\n")
